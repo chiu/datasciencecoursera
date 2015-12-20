b08cfcfbb936b7fdac668f7dc534871c424d3244
chiu <chiucode@gmail.com>
2015-12-19 23:52:05 -0800

    almost done part 2

--- 02-R-programming/week-2/assignment-2/pollutantmean.R
+++ 02-R-programming/week-2/assignment-2/pollutantmean.R
@@ -46,11 +46,54 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
 }
 
 
-test1 <-
-  pollutantmean(directory = 'specdata', pollutant = 'sulfate', id = 1:10)
+# test1 <-
+#   pollutantmean(directory = 'specdata', pollutant = 'sulfate', id = 1:10)
+# 
+# test2 <- 
+#   pollutantmean("specdata", "nitrate", 70:72)
+# 
+# test3 <-
+#   pollutantmean("specdata", "nitrate", 23)
 
-test2 <- 
-  pollutantmean("specdata", "nitrate", 70:72)
 
-test3 <-
-  pollutantmean("specdata", "nitrate", 23)
+complete <- function(directory, id = 1:332) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
+  
+  formatted_index <- formatC(id[1], width = 3, flag = "0")
+  raw_data <-
+    read.csv(paste0(directory,'/',formatted_index,'.csv'))
+  aggregate_table <- raw_data
+  
+  id_no_first_element <- id[-1]
+  if(length(id)>1){
+    for (specific_index in id_no_first_element) {
+      
+  
+      print(specific_index)
+      formatted_index <- formatC(specific_index, width = 3, flag = "0")
+      raw_data <-
+        read.csv(paste0(directory,'/',formatted_index,'.csv'))
+      aggregate_table <- rbind(aggregate_table, raw_data)
+      
+    }
+  }
+  print("hi")
+
+return(aggregate_table)
+
+ 
+}
+
+complete_table <- complete(directory = 'specdata', c(2,4,8,10,12))
