806f0d9aed1513620aedc7c1a28e0c196f6cacd1
chiu <chiucode@gmail.com>
2015-12-19 23:31:43 -0800

    done loop

--- 02-R-programming/week-2/assignment-2/pollutantmean.R
+++ 02-R-programming/week-2/assignment-2/pollutantmean.R
@@ -1,6 +1,7 @@
 
-something <- read.csv('specdata/307.csv')
 
+# something <- read.csv('specdata/307.csv')
+library(data.table)
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
@@ -15,4 +16,27 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
   ## NOTE: Do not round the result!
+  # aggregate_table <- data.table()
+  
+
+  formatted_index <- formatC(id[1], width = 3, flag = "0")
+  raw_data <-
+    read.csv(paste0(directory,'/',formatted_index,'.csv'))
+  aggregate_table <- raw_data
+  
+  for (count in 2:length(id)) {
+    # browser()
+    specific_index <- id[count]
+    print(specific_index)
+    formatted_index <- formatC(specific_index, width = 3, flag = "0")
+    raw_data <-
+      read.csv(paste0(directory,'/',formatted_index,'.csv'))
+    aggregate_table <- rbind(aggregate_table, raw_data)
+    
+  }
+  return(aggregate_table)
 }
+
+
+aggregate_table <-
+  pollutantmean(directory = 'specdata', pollutant = 'sulfate', id = 1:10)
