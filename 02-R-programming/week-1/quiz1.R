a865fb6f367d3e7d98d00de9969aad5dc27f7369
chiu <chiucode@gmail.com>
2016-01-10 14:25:23 -0800

    Updated with 2016 stuff.

--- 02-R-programming/week-1/quiz1.R
+++ 02-R-programming/week-1/quiz1.R
@@ -1,7 +1,15 @@
+# question 8 2016
+x <- list(2, "a", "b", TRUE)
+
+
+
 some_data <- read.csv("hw1_data.csv", header=TRUE)
 first_2_rows <- some_data[1:2,]
 first_2_rows
 
+#new question 14
+last_2_rows <- tail(some_data)
+print(last_2_rows)
 # question 15
 row_47 <- some_data[47,]
 print(row_47)
@@ -24,9 +32,9 @@ some_data_table <- data.table(some_data)
 # trial_2
 
 trial_3 <- subset(some_data_table, subset=Ozone>31 & Temp > 90, select=Solar.R)
-answer <- mean(trial_3$Solar.R)
+answer_q18 <- mean(trial_3$Solar.R)
 
-#Question 19
+#Question 19 mean of temp when month is 6
 answer_19 <- subset(some_data_table, subset = Month == 6 )
 answer_19_2 <- mean(answer_19$Temp)
 
